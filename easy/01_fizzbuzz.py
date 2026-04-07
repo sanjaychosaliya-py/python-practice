2c93b2706bacedb592f4dbf64731b3b3716a0883
Sanjay Chosaliya <sanjaychosaliya165@gmail.com>
2026-04-07 10:09:14 +0530

    Update 01_fizzbuzz.py

--- easy/01_fizzbuzz.py
+++ easy/01_fizzbuzz.py
@@ -1,4 +1,4 @@
-#Q1 — Print numbers 1–100, replace multiples of 3 with 'Fizz', 5 with 'Buzz', both with 'FizzBuzz'
+#Q1 -- Print numbers 1–100, replace multiples of 3 with 'Fizz', 5 with 'Buzz', both with 'FizzBuzz'
 i = 1
 while i <= 100:
     if ( i % 15 == 0):
